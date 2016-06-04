4f47ec065dd919c7e4d2f131eb0565803dd7ef5f
andreas hefti <anhefti@yahoo.de>
2016-06-04 14:32:27 +0200

    changed FrameBuffer format

--- src/main/java/com/inari/firefly/libgdx/GdxGraphicsImpl.java
+++ src/main/java/com/inari/firefly/libgdx/GdxGraphicsImpl.java
@@ -469,7 +469,7 @@ public final class GdxGraphicsImpl implements FFGraphics {
     private Viewport createVirtualViewport( View view ) {
         Rectangle bounds = view.getBounds();
         OrthographicCamera camera = new OrthographicCamera( bounds.width, bounds.height );
-        FrameBuffer frameBuffer = new FrameBuffer( Format.RGB565, (int) ( bounds.width * FBO_SCALER ), (int) ( bounds.height * FBO_SCALER ), false ) ;
+        FrameBuffer frameBuffer = new FrameBuffer( Format.RGB888, (int) ( bounds.width * FBO_SCALER ), (int) ( bounds.height * FBO_SCALER ), false ) ;
         TextureRegion textureRegion = new TextureRegion( frameBuffer.getColorBufferTexture() );
         textureRegion.flip( false, false );
         
