49f84fa50e057394f43700a577b272fa12b2e36a
andreas hefti <anhefti@yahoo.de>
2016-11-05 13:54:41 +0100

    code cleanup

--- src/main/java/com/inari/firefly/libgdx/GdxGraphicsImpl.java
+++ src/main/java/com/inari/firefly/libgdx/GdxGraphicsImpl.java
@@ -28,8 +28,6 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Pixmap.Format;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.Texture.TextureFilter;
-import com.badlogic.gdx.graphics.Texture.TextureWrap;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.graphics.glutils.FrameBuffer;
